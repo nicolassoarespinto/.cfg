bd22a55899231d11a260a8e90ebca05d2bc51674
Nicolas Pinto <nicolassoarespinto@hotmail.com>
2025-02-09 18:28:48 -0300

    avante

---
+++ nvim/lua/custom/lazy/avante.lua
@@ -0,0 +1,20 @@
+return {
+  "yetone/avante.nvim",
+  event = "VeryLazy",
+  lazy = false,
+  version = false, -- Set this to "*" to always pull the latest release version, or set it to false to update to the latest code changes.
+  opts = {
+    -- add any opts here
+    -- for example
+    provider="claude",
+    auto_suggestions_provider = "copilot",
+  },
+  -- if you want to build from source then do `make BUILD_FROM_SOURCE=true`
+  build = "make",
+  -- build = "powershell -ExecutionPolicy Bypass -File Build.ps1 -BuildFromSource false" -- for windows
+  dependencies = {
+    "stevearc/dressing.nvim",
+    "nvim-lua/plenary.nvim",
+    "MunifTanjim/nui.nvim",
+  },
+}
