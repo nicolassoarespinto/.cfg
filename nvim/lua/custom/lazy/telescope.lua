d2426dc3ecffc254ce406b12d2682142ec6c3f84
Nicolas Pinto <nicolassoarespinto@hotmail.com>
2025-02-09 13:35:32 -0300

    Organize telescope setup

--- nvim/lua/custom/lazy/telescope.lua
+++ nvim/lua/custom/lazy/telescope.lua
@@ -1,52 +1,22 @@
-return { 
+return {
     "nvim-telescope/telescope.nvim",
     dependencies = {
         "nvim-lua/plenary.nvim",
         "nvim-telescope/telescope-file-browser.nvim",
-        {"nvim-telescope/telescope-fzf-native.nvim",
-        build = "make"
+        {
+            "nvim-telescope/telescope-fzf-native.nvim",
+            build = "make"
         },
-        {"nvim-telescope/telescope-smart-history.nvim",
-        build = "make"
+        {
+            "nvim-telescope/telescope-smart-history.nvim",
+            build = "make"
+        },
+        {
+            "tami5/sqlite.lua",
         }
     },
-    config = function ()
-        local data = assert(vim.fn.stdpath "data")
-        require('telescope').load_extension('file_browser')
-        require('telescope').load_extension('fzf')
-        require('telescope').load_extension('smart_history')
-        require('telescope').setup{
-            defaults = {},
-            extensions = {
-                fzf = {
-                },
-                history = {
-                    path = vim.fs.joinpath(data, "telescope_history.sqlite3"),
-                    limit=100,
-                },
-            }
-        }
-        -- extensions ={
-        --         file_browser = {
-        --     theme = "ivy",
-        --     hijack_netrw = false
-        -- }
-        -- }
-        -- }
-
-        local builtin = require('telescope.builtin')
-        vim.keymap.set('n', '<leader>ff', function() builtin.find_files({ follow = false, path_display, path_display= {"absolute"} }) end, {})
-        vim.keymap.set('n', '<C-p>', builtin.find_files, {})
-        vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
-        vim.keymap.set('n', '<leader>fj', require "custom.telescope.multi-ripgrep", {})
-        vim.keymap.set('n', '<leader>ffg', builtin.git_files, {})
-        vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
-        vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
-        vim.keymap.set('n', '<leader>fk', builtin.keymaps, {})
-        --vim.keymap.set('n', '<C-p>', builtin.git_files, {})
-
-        vim.keymap.set('n', '<leader>ft', function() require('telescope').extensions.file_browser.file_browser() end, {})
-
+    config = function()
+        require "custom.telescope"
     end
 
 }
---
+++ nvim/lua/custom/telescope.lua
@@ -0,0 +1,62 @@
+
+local data = assert(vim.fn.stdpath "data")
+
+require('telescope').setup {
+        defaults = {},
+        extensions = {
+            fzf = { },
+            history = {
+                path = vim.fs.joinpath(data, "telescope_history.sqlite3"),
+                limit = 100,
+            },
+        }
+    }
+   
+pcall(require('telescope').load_extension('file_browser'))
+pcall(require('telescope').load_extension('fzf'))
+pcall(require('telescope').load_extension('smart_history'))
+
+local builtin = require('telescope.builtin')
+vim.keymap.set('n', '<leader>ff',
+    function()
+        builtin.find_files({
+            follow = false,
+            path_display = { "absolute" }
+        })
+    end)
+ vim.keymap.set('n', '<leader>f[',
+            function()
+                local opts = require('telescope.themes').get_ivy({
+                    cwd = vim.fn.stdpath("config"),
+                })
+                builtin.find_files(opts)
+    end)
+
+-- Search from home directorey
+vim.keymap.set('n', '<leader>f]',
+            function()
+                local ignore_patterns = {"miniconda", "anaconda"}
+                local opts = {
+                    cwd = vim.fn.expand("$HOME"),
+                    hidden = false,
+                    file_ignore_patterns = ignore_patterns,
+                    path_display = { "absolute" },
+                    previewer = false,
+
+                }
+                require('telescope.builtin').find_files(opts)
+    end)
+
+
+
+
+vim.keymap.set('n', '<C-p>', builtin.find_files, {})
+vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
+vim.keymap.set('n', '<leader>fj', require "custom.telescope.multi-ripgrep", {})
+vim.keymap.set('n', '<leader>ffg', builtin.git_files, {})
+vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
+vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
+vim.keymap.set('n', '<leader>fk', builtin.keymaps, {})
+
+vim.keymap.set('n', '<leader>ft', function() require('telescope').extensions.file_browser.file_browser() end, {})
+
