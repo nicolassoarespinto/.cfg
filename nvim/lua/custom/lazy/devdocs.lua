a2f8f7bec64bacedb3645a282c495c457399192d
Nicolas Pinto <nicolas.soares@genoacapital.com.br>
2025-11-01 11:13:34 -0300

    devdocs

---
+++ nvim/lua/custom/lazy/devdocs.lua
@@ -0,0 +1,54 @@
+  return {
+    "maskudo/devdocs.nvim",
+    dependencies = {
+      "folke/snacks.nvim",
+    },
+    cmd = { "DevDocs" },
+    keys = {
+      {
+        "<leader>ho",
+        mode = "n",
+        "<cmd>DevDocs get<cr>",
+        desc = "Get Devdocs",
+      },
+      {
+        "<leader>hi",
+        mode = "n",
+        "<cmd>DevDocs install<cr>",
+        desc = "Install Devdocs",
+      },
+      {
+        "<leader>hv",
+        mode = "n",
+        function()
+          local devdocs = require("devdocs")
+          local installedDocs = devdocs.GetInstalledDocs()
+          vim.ui.select(installedDocs, {}, function(selected)
+            if not selected then
+              return
+            end
+            local docDir = devdocs.GetDocDir(selected)
+            -- prettify the filename as you wish
+            Snacks.picker.files({ cwd = docDir })
+          end)
+        end,
+        desc = "Get Devdocs",
+      },
+      {
+        "<leader>hd",
+        mode = "n",
+        "<cmd>DevDocs delete<cr>",
+        desc = "Delete Devdoc",
+      }
+    },
+    opts = {
+      ensure_installed = {
+        "go",
+        "html",
+        "http",
+        "javascript",
+        "lua~5.1",
+        "python~3.13"
+      },
+    },
+  } 
