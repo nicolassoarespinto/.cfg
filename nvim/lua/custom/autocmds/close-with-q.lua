3deecb78a9a6e6578f810eca7af26d2d941e94b1
Nicolas Pinto <nicolas.soares@genoacapital.com.br>
2025-11-02 13:03:29 -0300

    autocmds

---
+++ nvim/lua/custom/autocmds/close-with-q.lua
@@ -0,0 +1,28 @@
+-- close some filetypes with <q>
+vim.api.nvim_create_autocmd('FileType', {
+  pattern = {
+    'PlenaryTestPopup',
+    'help',
+    'lspinfo',
+    'man',
+    'notify',
+    'qf',
+    'startuptime',
+    'tsplayground',
+    'checkhealth',
+    'neotest-summary',
+    'neotest-output-panel',
+    'toggleterm',
+    'blink-tree',
+    'gitsigns-blame',
+    'AvanteAsk',
+    'AvanteInput',
+    'markdown',
+  },
+  callback = function(event)
+    if bo.filetype ~= 'markdown' or bo.buftype == 'help' then
+      -- bo.buflisted = false
+      vim.keymap.set('n', 'q', '<cmd>close<cr>', { buffer = event.buf, silent = true, nowait = true })
+    end
+  end,
+})
