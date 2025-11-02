a7d76f1efd1005ca6410ae4718ef9a364942019f
Nicolas Pinto <nicolas.soares@genoacapital.com.br>
2025-11-02 12:35:14 -0300

    configure more telescope pickers

--- nvim/lua/custom/lazy/telescope.lua
+++ nvim/lua/custom/lazy/telescope.lua
@@ -1,23 +1,77 @@
 return {
-  "nvim-telescope/telescope.nvim",
-  dependencies = {
-    "nvim-lua/plenary.nvim",
-    "nvim-telescope/telescope-file-browser.nvim",
+
     {
-      "nvim-telescope/telescope-fzf-native.nvim",
-      build = "make"
+        "nvim-telescope/telescope.nvim",
+        dependencies = {
+            "nvim-lua/plenary.nvim",
+            "nvim-telescope/telescope-file-browser.nvim",
+            {
+                "nvim-telescope/telescope-fzf-native.nvim",
+                build = "make"
+            },
+            {
+                "nvim-telescope/telescope-smart-history.nvim",
+                build = "make"
+            },
+            {
+                "tami5/sqlite.lua",
+            }
+        },
+        config = function()
+            require "custom.telescope"
+            require "custom.telescope_dbui"
+        end,
     },
+    -- smarter fuzzy search
     {
-      "nvim-telescope/telescope-smart-history.nvim",
-      build = "make"
+        'nvim-telescope/telescope-fzy-native.nvim',
+        lazy = true,
+        dependencies = {
+            {
+                'romgrk/fzy-lua-native',
+                build = {
+                    'make',
+                    -- otherwise lazy.nvim will complain on checkout
+                    -- https://github.com/romgrk/fzy-lua-native/issues/23
+                    'git update-index --assume-unchanged static/libfzy-*.so',
+                },
+            },
+            'nvim-telescope/telescope.nvim',
+        },
+        config = function()
+            vim.schedule(function() require('telescope').load_extension('fzy_native') end)
+        end,
     },
+
+
     {
-      "tami5/sqlite.lua",
+        'bassamsdata/namu.nvim',
+        keys = {
+            {
+                '<leader>ss',
+                function() require('namu.namu_symbols').show() end,
+                desc = 'Symbols (Document)',
+            },
+        },
+        opts = {
+            namu_symbols = {
+                enable = true,
+                options = {
+                    display = { mode = 'icon' },
+                    -- window = { border = { ' ' } },
+                    AllowKinds = {
+                        rust = {
+                            'Function',
+                            'Struct',
+                            'Enum',
+                            'Trait',
+                            'Module',
+                        },
+                    },
+                },
+            },
+            ui_select = { enable = true },
+        },
     }
-  },
-  config = function()
-    require "custom.telescope"
-    require "custom.telescope_dbui"
-  end
 
 }
--- nvim/lua/custom/telescope.lua
+++ nvim/lua/custom/telescope.lua
@@ -1,14 +1,14 @@
 local data = assert(vim.fn.stdpath "data")
 
 require('telescope').setup {
-  defaults = {},
-  extensions = {
-    fzf = {},
-    history = {
-      path = vim.fs.joinpath(data, "telescope_history.sqlite3"),
-      limit = 100,
-    },
-  }
+    defaults = {},
+    extensions = {
+        fzf = {},
+        history = {
+            path = vim.fs.joinpath(data, "telescope_history.sqlite3"),
+            limit = 100,
+        },
+    }
 }
 
 pcall(require('telescope').load_extension('file_browser'))
@@ -17,39 +17,39 @@ pcall(require('telescope').load_extension('smart_history'))
 
 local builtin = require('telescope.builtin')
 vim.keymap.set('n', '<leader>ff',
-  function()
-    builtin.find_files({
-      follow = false,
-      path_display = { "absolute" }
-    })
-  end)
+    function()
+        builtin.find_files({
+            follow = false,
+            path_display = { "absolute" }
+        })
+    end)
 vim.keymap.set('n', '<leader>f[',
-  function()
-    local opts = require('telescope.themes').get_ivy({
-      cwd = vim.fn.stdpath("config"),
-    })
-    builtin.find_files(opts)
-  end)
+    function()
+        local opts = require('telescope.themes').get_ivy({
+            cwd = vim.fn.stdpath("config"),
+        })
+        builtin.find_files(opts)
+    end)
 
 -- Search from home directorey
 vim.keymap.set('n', '<leader>f]',
-  function()
-    local ignore_patterns = { "miniconda", "anaconda" }
-    local opts = {
-      cwd = vim.fn.expand("$HOME"),
-      hidden = false,
-      file_ignore_patterns = ignore_patterns,
-      path_display = { "absolute" },
-      previewer = false,
+    function()
+        local ignore_patterns = { "miniconda", "anaconda" }
+        local opts = {
+            cwd = vim.fn.expand("$HOME"),
+            hidden = false,
+            file_ignore_patterns = ignore_patterns,
+            path_display = { "absolute" },
+            previewer = false,
 
-    }
-    require('telescope.builtin').find_files(opts)
-  end)
+        }
+        require('telescope.builtin').find_files(opts)
+    end)
 
 
 local telescope_meta_picker = function()
-  -- Picker for telescope pickers
-  pickers = {}
+    -- Picker for telescope pickers
+    pickers = {}
 end
 
 
@@ -60,10 +60,19 @@ vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
 vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
 vim.keymap.set('n', '<leader>fk', builtin.keymaps, {})
 vim.keymap.set('n', '<leader>fs', builtin.lsp_references, {})
-vim.keymap.set('n',  '<leader>ffj', builtin.git_status, {})
+vim.keymap.set('n', '<leader>ffj', builtin.git_status, {})
 vim.keymap.set('n', '<leader>f]',
-  function()
-    local opts = { previewer = false }
-    builtin.builtin(opts)
-  end
+    function()
+        local opts = { previewer = false }
+        builtin.builtin(opts)
+    end
 )
+vim.keymap.set('n', '<leader>f;', '<cmd>Telescope commands<cr>', { desc = 'Commands' })
+vim.keymap.set('n', '<leader>fc', '<cmd>Telescope command_history<cr>', { desc = 'Command History' })
+vim.keymap.set('n', '<leader>fm', '<cmd>Telescope marks<cr>', { desc = 'Jump to Mark' })
+vim.keymap.set('n', '<leader>f/', function()
+    require('telescope.builtin').current_buffer_fuzzy_find(require('telescope.themes').get_dropdown({
+        winblend = 10,
+        previewer = false,
+    }))
+end, { desc = 'Search Current Buffer' })
