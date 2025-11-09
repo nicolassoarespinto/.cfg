301ad855f9713f6cd011e1614681470e37c26e55
Nicolas Pinto <10147479+nicolassoarespinto@users.noreply.github.com>
2025-11-09 13:55:10 -0300

    tmux appimage

---
+++ scripts/install_tmux.sh
@@ -0,0 +1,9 @@
+curl -s https://api.github.com/repos/nelsonenzo/tmux-appimage/releases/latest \
+| grep "browser_download_url.*appimage" \
+| cut -d : -f 2,3 \
+| tr -d \" \
+| wget -qi - \
+&& chmod +x tmux.appimage
+
+## optionaly, move it into your $PATH
+mv tmux.appimage ~/.local/bin/tmux
