6b3f1a7ae3e9265bd205f7b4be34404514e9167e
Nicolas Pinto <10147479+nicolassoarespinto@users.noreply.github.com>
2025-11-09 16:38:58 -0300

    fix cdf.sh

--- bash/cdf.sh
+++ bash/cdf.sh
@@ -1,39 +1,36 @@
 cdf() {
-  # Ignored directory patterns (edit freely)
-  local ignore_patterns=(
-    "__pycache__", "miniconda" 
-  )
+# Ignored directory patterns (edit freely)
+local ignore_patterns=(
+  "__pycache__", "miniconda", ".venv",
+)
 
-  # Build ignore options for fzf --walker-ignore
-  local ignore_opts=()
-  for pat in "${ignore_patterns[@]}"; do
-    ignore_opts+=(--walker-ignore "$pat")
-  done
+# Build skip list for fzf
+local skip_list
+skip_list=$(IFS=,; echo "${ignore_patterns[*]}")
 
-  # Choose root: use argument if given, otherwise current dir
-  local root="${1:-.}"
+# Choose root: use argument if given, otherwise current dir
+local root="${1:-.}"
 
-  # You can toggle between 'pretty' and 'simple' modes here
-  local mode="pretty"
+# You can toggle between 'pretty' and 'simple' modes here
+local mode="pretty"
 
-  if [[ $mode == "pretty" ]]; then
-    cd "$(
-      fzf \
-        --walker dir \
-        --walker-root "$root" \
-        "${ignore_opts[@]}" \
-        --preview 'ls --color=always -A {}' \
-        --height=40% \
-        --reverse \
-        --border
-    )" || return
-  else
-    cd "$(
-      fzf \
-        --walker dir \
-        --walker-root "$root" \
-        "${ignore_opts[@]}"
-    )" || return
-  fi
+if [[ $mode == "pretty" ]]; then
+  cd "$(
+    fzf \
+      --walker dir \
+      --walker-root "$root" \
+      --walker-skip "$skip_list" \
+      --preview 'ls --color=always -A {}' \
+      --height=40% \
+      --reverse \
+      --border
+  )" || return
+else
+  cd "$(
+    fzf \
+      --walker dir \
+      --walker-root "$root" \
+      --walker-skip "$skip_list"
+  )" || return
+fi
 }
-
