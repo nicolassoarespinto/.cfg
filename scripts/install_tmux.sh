f0767db3a1fc4b3b47ab15aa7936edc46c283470
Nicolas Pinto <10147479+nicolassoarespinto@users.noreply.github.com>
2025-11-09 14:05:34 -0300

    other

--- scripts/install_tmux.sh
+++ scripts/install_tmux.sh
@@ -1,3 +1,8 @@
+## TPM
+git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+
+
+## Get appimage of latest release
 curl -s https://api.github.com/repos/nelsonenzo/tmux-appimage/releases/latest \
 | grep "browser_download_url.*appimage" \
 | cut -d : -f 2,3 \
@@ -5,5 +10,7 @@ curl -s https://api.github.com/repos/nelsonenzo/tmux-appimage/releases/latest \
 | wget -qi - \
 && chmod +x tmux.appimage
 
-## optionaly, move it into your $PATH
+## move it into your $PATH
 mv tmux.appimage ~/.local/bin/tmux
+
+
