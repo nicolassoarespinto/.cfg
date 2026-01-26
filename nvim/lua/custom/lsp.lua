a24338baa46614b56c075556535ab691f6e23a69
Nicolas Pinto <10147479+nicolassoarespinto@users.noreply.github.com>
2026-01-26 16:10:45 -0300

    lsp

--- nvim/lua/custom/lsp.lua
+++ nvim/lua/custom/lsp.lua
@@ -169,6 +169,7 @@ function M.lsp_servers()
     markdown_oxide = true,
     pylsp = false,
     ty = true,
+    ruff = true,
     basedpyright = false,
     lua_ls = true,
   }
@@ -210,6 +211,53 @@ function M.setup()
     end
   end
 
+  vim.api.nvim_create_autocmd("LspAttach", {
+    callback = function(args)
+      local bufnr = args.buf
+      local client = assert(vim.lsp.get_client_by_id(args.data.client_id), "must have valid client")
+
+      if client.name == "ruff" then
+        client.server_capabilities.hoverProvider = false
+      end
+
+      vim.opt_local.omnifunc = "v:lua.vim.lsp.omnifunc"
+      vim.keymap.set("n", "K", vim.lsp.buf.hover, { buffer = bufnr })
+      vim.keymap.set("n", "gd", "<cmd>lua vim.lsp.buf.definition()<CR>", { buffer = bufnr })
+      vim.keymap.set("n", "[w", "<cmd>lua vim.lsp.buf.workspace_symbol()<CR>", { buffer = bufnr })
+      vim.keymap.set("n", "<leader>vd", "<cmd>lua vim.diagnostic.open_float()<CR>", { buffer = bufnr })
+      vim.keymap.set("n", "[d", "<cmd>lua vim.diagnostic.jump({count=1})<CR>", { buffer = bufnr })
+      vim.keymap.set("n", "]d", "<cmd>lua vim.diagnostic.jump({count=-1})<CR>", { buffer = bufnr })
+      vim.keymap.set("n", "<leader>[a", "<cmd>lua vim.lsp.buf.code_action()<CR>", { buffer = bufnr })
+      vim.keymap.set("n", "<F2>", "<cmd>lua vim.lsp.buf.rename()<CR>", { buffer = bufnr })
+      vim.keymap.set("i", "<C-h>", "<cmd>lua vim.lsp.buf.signature_help()<CR>", { buffer = bufnr })
+      vim.keymap.set({ "n", "x" }, "<F3>", "<cmd>lua vim.lsp.buf.format({async = true})<CR>",
+        { buffer = bufnr })
+      if pcall(require, "namu.namu_symbols") then
+        vim.keymap.set("n", "<leader>sn", function() require("namu.namu_symbols").show() end,
+          { buffer = bufnr, desc = "Symbols (Document)" })
+
+        vim.keymap.set("n", "<leader>sd", function() require("namu.namu_diagnostics").show() end,
+          { buffer = bufnr, desc = "Symbols (Document)" })
+      end
+    end,
+  })
+
+  vim.g.diagnostics_active = false
+  vim.diagnostic.enable(false)
+
+  local function toggle_diagnostics()
+    if vim.g.diagnostics_active then
+      vim.g.diagnostics_active = false
+      vim.diagnostic.enable(false)
+    else
+      vim.g.diagnostics_active = true
+      vim.diagnostic.enable(true)
+    end
+  end
+
+  vim.keymap.set("n", "<leader>xd", toggle_diagnostics,
+    { noremap = true, silent = true, desc = "Toggle vim diagnostics" })
+  vim.keymap.set("n", "<leader>bf", ":lua vim.lsp.buf.format()<CR>", { desc = "Format buffer" })
 
   -- Set global capabilities for all LSP servers
   -- vim.lsp.config("*", {
